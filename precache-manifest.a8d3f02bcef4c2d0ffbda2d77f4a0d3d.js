a74fffa9efd3e43fe6df7feb7aac9a3ebd54ecdf
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-08-19 17:51:26 +0600

    Updates

---
+++ precache-manifest.a8d3f02bcef4c2d0ffbda2d77f4a0d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce334591d50565190bc85abde884b76c",
+    "url": "/09-amazone-clone/index.html"
+  },
+  {
+    "revision": "2f2552a32402a3f5fa1d",
+    "url": "/09-amazone-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2d00f9992169acc52b9",
+    "url": "/09-amazone-clone/static/js/2.cb1d127d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/09-amazone-clone/static/js/2.cb1d127d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2552a32402a3f5fa1d",
+    "url": "/09-amazone-clone/static/js/main.4e5ad2d2.chunk.js"
+  },
+  {
+    "revision": "92f0f026520d875e6bfd",
+    "url": "/09-amazone-clone/static/js/runtime-main.f0ee3795.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/09-amazone-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
